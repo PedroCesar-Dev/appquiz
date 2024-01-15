d6858e33907589ef59368474e9a49a66c4a842fb
Pedro Cesar <119058413+PedroCesar-Dev@users.noreply.github.com>
2024-01-15 17:07:58 -0300

    New quiz project

---
+++ src/Data/perguntas.js
@@ -0,0 +1,23 @@
+export const Perguntas  = [
+    {
+        pergunta:'Qual século estamos?',
+        opcoesResposta:[
+            {resposta:'20', correta:false, alternativa:'A)'},
+            {resposta:'22', correta:false, alternativa:'B)'},
+            {resposta:'21', correta:true, alternativa:'C)'},
+            {resposta:'19', correta:false, alternativa:'D)'},
+        ]
+    },
+    
+    {
+        pergunta:'Onde está localizado o rio Tiête?',
+        opcoesResposta:[
+            {resposta:'Minas Gerais', correta:false, alternativa:'A)'},
+            {resposta:'São Paulo', correta:true, alternativa:'B)'},
+            {resposta:'Santos', correta:false, alternativa:'C)'},
+            {resposta:'Maranhão', correta:false, alternativa:'D)'},
+        ]
+    }
+    
+    ]
+    
